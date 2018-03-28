dad2c0d99bc1cbe1c8542ef04e492a2269b41e48
MrOtherGuy <jastekken@outlook.com>
2018-03-28 09:58:28 +0300

    Add some comments and format SBtabs better

--- background-script.js
+++ background-script.js
@@ -1,15 +1,18 @@
 "use strict;"
 
 browser.runtime.onInstalled.addListener(setDefault);
+// Set default options
 function setDefault() {
 
   var gettingItem = browser.storage.local.get(["menuOptions"]);
   gettingItem.then((res) => {
 		var options = res.menuOptions;
+		console.log(options);
 		var names = options ? Object.getOwnPropertyNames(options) : [];
+		console.log(names);
 		if (!(names.includes("changeTabKey")
 					&& names.includes("muteKey")
-					&& names.inludes("pauseKey"))){
+					&& names.includes("pauseKey"))){
 			browser.storage.local.set({
 				menuOptions:{
 					changeTabKey: undefined,
@@ -22,20 +25,49 @@ function setDefault() {
 }
 
 let soundman = new function(){
-	this.getTabs = ()=>{return SBtabs.tabs};
+	// getTabs is for debug purposes
+	this.getTabs = () => { return SBtabs.tabs };
+	// Current options
 	var options = {undefinedKey:"switch",CtrlKey:"playback",ShiftKey:"mute"};
+	// Manager to store tab states
 	var SBtabs = new function(){
-		this.tabs = {},
-		this.prevTab = new function(){this.clear=()=>{this.title=null,this.id=null,this.fromTab=null};this.title=null,this.id=null,this.fromTab=null};
+		this.tabs = {};
+		this.prevTab = new function(){
+			this.clear = () => {
+				this.title = null;
+				this.id = null;
+				this.fromTab = null
+			};
+			this.title = null;
+			this.id = null;
+			this.fromTab = null
+		};
 		//this.isPausing = (id) => {return this.get(id).pausing};
-		this.set = (tab,property,value) => { if(value === true || value === false){tab[property] = value} },
-		this.get = (id)=>{return this.tabs[id]},
-		this.put = (id)=>{this.tabs[id]={muted:false,audible:true,pinned:false,paused:false}; createMenu(id);return this.get(id)},
-		this.remove = (id)=>{delete this.tabs[id]}
+		this.set = (tab, property, value) => {
+			if(value === true || value === false){
+				tab[property] = value
+			}
+		};
+		this.get = (id) => { return this.tabs[id] };
+		this.put = (id) => {
+			this.tabs[id] = {
+				muted:false,
+				audible:true,
+				pinned:false,
+				paused:false
+				};
+			createMenu(id);
+			return this.get(id)
+		};
+		this.remove = (id) => { delete this.tabs[id] };
 
 	};
-	
+	// Validate and map click modifiers
 	function setOptions(opt){
+		if(!opt){
+			//This can happen on first install, but options is initialized to correct values at that time anyway
+			return
+		}
 		var valids = [undefined,"Ctrl","Shift"];
 		var diff = ((opt.changeTabKey != opt.pauseKey)
 									&& (opt.changeTabKey != opt.muteKey)
@@ -45,9 +77,6 @@ let soundman = new function(){
 			&& valids.includes(opt.changeTabKey)
 			&& valids.includes(opt.pauseKey)
 			&& valids.includes(opt.muteKey)){
-			/*options.changeTabKey = opt.changeTabKey;
-			options.pauseKey = opt.pauseKey;
-			options.muteKey = opt.muteKey;*/
 			options[opt.changeTabKey+"Key"] = "switch";
 			options[opt.pauseKey+"Key"] = "playback";
 			options[opt.muteKey+"Key"] = "mute";
@@ -57,12 +86,14 @@ let soundman = new function(){
 		}
 	}
 	
-	
+	// This is only listening when we change tab from within context menu
+	// If the user changes a tab himself we want to clear the previous tab store
 	function handleActiveChanged(tabInfo){
 		SBtabs.prevTab.clear();
 		browser.tabs.onActivated.removeListener(handleActiveChanged);
 	}
-	
+	// Filter out uninteresting changes
+	// This doesn't fire when active tab changes
 	function determineAction(tabId,changeInfo, tabInfo){
 		if(changeInfo.audible === undefined && changeInfo.mutedInfo === undefined){
 				return 0
@@ -72,7 +103,7 @@ let soundman = new function(){
 		}
 		return 0
 	}
-	
+	// Handle changes to tabs
 	function handleUpdated(tabId,changeInfo){
 		
 		var tab = SBtabs.get(tabId) || SBtabs.put(tabId);
@@ -93,6 +124,7 @@ let soundman = new function(){
 			handleRemoved(tabId);
 		}
 	}
+	// Create a menuitem with associated id
 	let createMenu = (id) => {
 		browser.menus.create({
 		id: "Soundman-" + id,
@@ -101,15 +133,17 @@ let soundman = new function(){
 		icons: {"16":"Soundman16.png"}
 		});
 	}
+	// Remove menuitem for this tab
 	let removeMenu = (id) => { browser.menus.remove("Soundman-"+id) }
 	
+	// Remove from manager
 	let handleRemoved = (tabId) => {
 		if(SBtabs.get(tabId)){
 			SBtabs.remove(tabId);
 			removeMenu(tabId);
 		}
 	}
-	
+	// Pin tab
 	let handlePinned = (tabId) => {
 		var tab = SBtabs.get(tabId);
 		if(tab && tab.pinned){
@@ -121,97 +155,104 @@ let soundman = new function(){
 			SBtabs.set(tab,"pinned",true);
 		}
 	}
-	
+	// Unpin tab
 	let unpin = (tabId) => {
 		var tab = SBtabs.get(tabId);
 		SBtabs.set(tab,"pinned",false);
 		if(!tab.muted && !tab.paused && !tab.audible){
-			//console.log(changeInfo);
 			handleRemoved(tabId);
 		}
 		
 	}
 	
-	let updateMenu = (info,tab) => {
-		
+	let selectMenuUpdate = (info,tab) => {
+		// Handle tab context menu here
 		if(info.contexts[0] === "tab"){
-			var SBtab = SBtabs.get(tab.id);
-			if(SBtab){
+			if(SBtabs.get(tab.id)){
 				browser.menus.update("SoundmanDelete",{enabled:true});
 				SBtab.pinned && browser.menus.update("SoundmanPin",{title:"Unpin from Soundman"});
 				browser.menus.refresh();
 				browser.menus.update("SoundmanDelete",{enabled:false});
 				browser.menus.update("SoundmanPin",{title:"Pin to Soundman"});
 			}
-		}else{
+		}else if(info.contexts[0]==="page"){
+			updateMenu();
+		}
+		return 0
+	}
+	// Update content context menu
+	let updateMenu = () => {
 		
-			var promises = [];
-			for (var id in SBtabs.tabs){
-				promises.push(browser.tabs.get(+id));
-			}
-			Promise.all(promises).then((tabs) => {
-				for(tab of tabs){
-					var title = "";
-					if(SBtabs.prevTab.fromTab === tab.id){
-						title += "🔁 "+SBtabs.prevTab.title;
-					}else{
-						var mute = ["🔊","🔇"][+SBtabs.get(tab.id).muted];
-						var play = ["⏸ ","▶️ "][+SBtabs.get(tab.id).paused];
-						var pin = ["","📌"][+SBtabs.get(tab.id).pinned];
-						title += (pin + mute + play + tab.title);
-					}
-					browser.menus.update("Soundman-"+tab.id,{
-					title: title,
-					enabled: true
-					});
+		var promises = [];
+		for (var id in SBtabs.tabs){
+			promises.push(browser.tabs.get(+id));
+		}
+		if (promises.length === 0){
+			return 0
+		}
+		Promise.all(promises).then((tabs) => {
+			for(tab of tabs){
+				var title;
+				if(SBtabs.prevTab.fromTab === tab.id){
+					title = "🔁 " + SBtabs.prevTab.title;
+				}else{
+					var SBtab = SBtabs.get(tab.id);
+					title = ["","📌"][+SBtab.pinned] + ["🔊","🔇"][+SBtab.muted] + ["▶️ ","⏸ "][+SBtab.paused] + tab.title;
 				}
-				browser.menus.refresh();
-			});
-		}
+				browser.menus.update("Soundman-"+tab.id,{
+				title: title,
+				enabled: true
+				});
+			}
+			
+		}).then(()=>{browser.menus.refresh();});
 	}
+	// map modifier keys to actions
 	let getMenuAction = (mods) => {
-		
 		return options[mods[0]+"Key"]
-		/*
-		var retval = "switch";
-		if(mods.includes("Ctrl")){
-			retval = "playback";
-		}else if(mods.includes("Shift")){
-			retval = "mute";
-		}
-		return retval*/
 	}
-	
+	// Playback control
 	let playback = (id) => {
 		var tab = SBtabs.get(id);
 		var state = tab.paused;
+		// Select the function
 		var func = state ? ".play();": ".pause();";
+		// Construct and send this script to content window
+		// Page specific functionality is sadness but necessary
 		browser.tabs.executeScript(id,{
 			code: "(function(){var state="+!state+";var media=document.getElementsByTagName('video')[0]||document.getElementsByTagName('audio')[0]||null;if(media!=null){media && media"+func+"}else{media=document.getElementsByClassName('playControl')[0]||null;media&&media.tagName==='BUTTON'&&media.click()}browser.runtime.sendMessage({elem:!!media,state:state})})()",
 			allFrames:true
 		});
 		return 0
 	}
+	// Mute this tab
 	let mute = (tab)=>{
 		browser.tabs.update(tab,{muted:!(SBtabs.get(tab).muted)})
 	}
-
+	// Select which action to take on message
 	let handleMessage = (request,sender,sendResponse) => {
+		// Don't care about other senders
 		if(sender.id != "soundman@example.com"){
 			return
 		}
+		// Feedback from tab content
 		if(sender.envType === "content_child"){
 		
-		var tab = SBtabs.get(sender.tab.id);
-		if (tab.paused != request.state){
-			SBtabs.set(tab,"paused",!!(request.elem^tab.paused));
-		}
+			var tab = SBtabs.get(sender.tab.id);
+			if (tab.paused != request.state){
+				SBtabs.set(tab,"paused",!!(request.elem^tab.paused));
+			}
 		}else{
+			// update options based on message from options document
 			setOptions(request.SBOptions);
 		}
 	}
+	// Set options on startup
+	// On first
 	browser.storage.local.get(["menuOptions"]).then((options) => {setOptions(options.menuOptions)});
+	// Listen to tab changes
 	browser.tabs.onUpdated.addListener(determineAction);
+	// Remove the tab from soundman if the tab was closed
 	browser.tabs.onRemoved.addListener(handleRemoved);
 	// Create delete menu
 	browser.menus.create({
@@ -228,6 +269,7 @@ let soundman = new function(){
 		contexts: ["tab"],
 		icons: {"16":"Soundman16.png"},
 	});
+	// Select which tabId should switch to
 	let getSwitchTabId = (newId,curId,curTitle) => {
 		var retval;
 		if (newId != curId){
@@ -240,8 +282,11 @@ let soundman = new function(){
 		}
 		return retval
 	}
-	browser.menus.onShown.addListener(updateMenu);
+	// Update menus when shown
+	browser.menus.onShown.addListener(selectMenuUpdate);
+	// Listen to feedback from content and options
 	browser.runtime.onMessage.addListener(handleMessage);
+	// Action for different menuitems
 	browser.menus.onClicked.addListener((menus, tab) => {
 	
 		if(menus.menuItemId === "SoundmanDelete"){
@@ -249,7 +294,7 @@ let soundman = new function(){
 		}else if(menus.menuItemId === "SoundmanPin"){
 			handlePinned(tab.id);
 		}else{
-			var tabId = +(menus.menuItemId.split("-")[1]); //This shit FIX
+			var tabId = +(menus.menuItemId.split("-")[1]);
 			var method = getMenuAction(menus.modifiers);
 			switch (method){
 				case "playback":
